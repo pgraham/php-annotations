a6fdc51f067cd2bc618bdc6d103edf65303693da
Philip Graham <philip.robert.graham@gmail.com>
2022-03-19 17:19:13 +0000

    remove license headers for source files in favour of a single license file at the repo root

--- test/AnnotationParsingTest.php
+++ test/AnnotationParsingTest.php
@@ -1,17 +1,4 @@
 <?php
-/**
- * =============================================================================
- * Copyright (c) 2022, Philip Graham
- * All rights reserved.
- *
- * This file is part of php-annotations and is licensed by the Copyright holder
- * under the 3-clause BSD License.  The full text of the license can be found in
- * the LICENSE.txt file included in the root directory of this distribution or
- * at the link below.
- * =============================================================================
- *
- * @license http://www.opensource.org/licenses/bsd-license.php
- */
 use zpt\anno\Annotations;
 use PHPUnit\Framework\TestCase as TestCase;
 
--- zpt/anno/AnnotationFactory.php
+++ zpt/anno/AnnotationFactory.php
@@ -1,17 +1,4 @@
 <?php
-/**
- * =============================================================================
- * Copyright (c) 2013, Philip Graham
- * All rights reserved.
- *
- * This file is part of Reed and is licensed by the Copyright holder under
- * the 3-clause BSD License.	The full text of the license can be found in the
- * LICENSE.txt file included in the root directory of this distribution or at
- * the link below.
- * =============================================================================
- *
- * @license http://www.opensource.org/licenses/bsd-license.php
- */
 namespace zpt\anno;
 
 use \ReflectionClass;
